eb4be5aeb6ed6987cdee35d019957b7f8e41c7c1
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-09 16:36:58 +0200

    Add list

--- magazyn.py
+++ magazyn.py
@@ -1,4 +1,45 @@
+items ={
+    "name" : ["Milk", "Sugar", "Flour", "Coffee"],
+    "quantity" : [120, 1000, 12000, 25],
+    "unit" : ["l", "kg", "kg", "kg"],
+    "unit_price" : [2.3, 3, 1.2, 40]
+}
+
+name = items.get("name")
+quantity = items.get("quantity")
+unit = items.get("unit")
+unit_price = items.get("unit_price")
+
+
+
+name.insert(0, "----")
+quantity.insert(0, "--------")
+unit.insert(0, "----")
+unit_price.insert(0, "----------")
+name.insert(0, "Name")
+quantity.insert(0, "Quantity")
+unit.insert(0, "Unit")
+unit_price.insert(0, "Unit Price")
+#print(name)
+
+def get_items():
+    #print("Name"+'\t'+"Quantity"+'\t'+"Unit"+'\t'+"Unit Price (PLN)"+'\n')
+    for i in range (len(name)):
+        print( str(name[i])+'\t'+ str(quantity[i])+'\t'+ str(unit.rjust(10)[i]) +'\t' + str(unit_price[i])+ '\n')
+
+    
+    
+
+
+
 input("What you like to do?")
+
+while "show":
+    
+    get_items()
+    exit()
+
+
 
 while "exit":
     print("Exiting... Bye!")
